54929f23d9c0ecd1d5718352cc5c75eb0c7f613f
maxembo <91334358+maxembo@users.noreply.github.com>
2023-01-21 09:55:41 +0300

    Add generating mesh

---
+++ Source/GeneratingRooms/GeneratingRooms.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GeneratingRooms.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GeneratingRooms, "GeneratingRooms" );
---
+++ Source/GeneratingRooms/GeneratingRoomsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GeneratingRoomsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENERATINGROOMS_API AGeneratingRoomsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
